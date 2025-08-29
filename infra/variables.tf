6485c7fa588381ae284f74b8a7973abda988beca
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-08-29 08:10:04 +0530

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,3 @@
+variable "project" { type = string  default = "hello-eks" }
+variable "region"  { type = string  default = "ap-south-1" } # Mumbai (pick your region)
+variable "eks_version" { type = string default = "1.29" }
